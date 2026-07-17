a652464f43bf7fb238cca5815171fa99bea9ecb5
Mohammad Nabiha <nnabiha676@gmail.com>
2026-07-17 10:59:19 +0530

    Add files via upload

---
+++ DAY 16/ATM/logic.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+data={
+    123456:{'pin':1234,'balance':5000,'name':'charan','history':[]},
+    234561:{'pin':1934,'balance':125000,'name':'kiran','history':[]},
+    345621:{'pin':1734,'balance':6000,'name':'Taran','history':[]},
+    
+def login():
+    acc_num=int(input("Enter the account number:"))
+    pin=int(input("Enter the pin:"))
+     if acc_num in data and data[
