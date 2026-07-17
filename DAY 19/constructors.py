508dc1f3c20270bc70b8e56690c9a2cc0a28b0a6
Mohammad Nabiha <nnabiha676@gmail.com>
2026-07-17 11:03:50 +0530

    Add files via upload

---
+++ DAY 19/constructors.py
@@ -0,0 +1,7 @@
+class Flipkart:
+    def __init__(self,name,phno):
+        self.username = name
+        self.phonenumber = phno
+        print(f'welcome to flipkart {self.username},shop now!!!')
+
+saniya = Flipkart('saniya','987654321')
